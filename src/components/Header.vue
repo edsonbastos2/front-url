03e18b6a1386957c27e41eb351455cd416fe2a23
Edson Bastos <edsonbastos2@hotmail.com>
2022-02-24 23:55:09 -0300

    Front-url

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Header',
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
